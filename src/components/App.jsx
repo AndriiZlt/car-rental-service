62dcc2684278d8ca80891f0fd430ffd6ccda97c0
AndriiZlt <andreyzlot@gmail.com>
2023-10-11 14:28:23 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="catalog" element={<Catalog />} />
         <Route path="favorites" element={<Cart />} />
-        <Route path="/*" element={<Navigate to="/" />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
   );
