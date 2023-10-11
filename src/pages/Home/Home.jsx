82ec85de3268d854a45c2fbd006e197eb4816b57
AndriiZlt <andreyzlot@gmail.com>
2023-10-11 18:53:56 +0300

    hero updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,61 +7,104 @@ import hero3 from 'assets/hero3.svg';
 import { useDispatch } from 'react-redux';
 import { setActivePage } from 'redux/cars-slice';
 
-var images = [hero1, hero2, hero3];
-
 const Home = () => {
   const TEL_NUMBER = process.env.REACT_APP_TEL;
-  const [image, setImage] = useState(images[0]);
+
+  const [opacity1, setOpacity1] = useState(1);
+  const [opacity2, setOpacity2] = useState(0);
+  const [opacity3, setOpacity3] = useState(0);
+  const [show, setShow] = useState(1);
+  const [margin, setMargin] = useState(0);
 
   const dispatch = useDispatch();
   useEffect(() => {
+    const div = document.getElementById('image1');
+    const { height } = div.getBoundingClientRect();
+    setMargin(height - 127);
+
     dispatch(setActivePage('home'));
+
     const heroFun = () => {
-      images.push(images.shift());
-      setImage(images[0]);
+      if (show === 1) {
+        setOpacity1(0);
+        setOpacity2(1);
+        setShow(2);
+      } else if (show === 2) {
+        setOpacity2(0);
+        setOpacity3(1);
+        setShow(3);
+      } else {
+        setOpacity1(1);
+        setOpacity3(0);
+        setShow(1);
+      }
     };
     let intervalId = setInterval(() => heroFun(), 5000);
     return () => clearInterval(intervalId);
-  });
+  }, [dispatch, show]);
 
   return (
-    <section className={css.section}>
-      <div className={css.hero}>
-        <img src={image} alt="hero" className={css.heroImage}></img>
-        <div className={css.info}>
-          <h1>DriveEasy Rental</h1>
-          <h2>The best car rental service in the world </h2>
-          <p className={css.text}>
-            Discover freedom on four wheels with DriveEasy Rental
-          </p>
-          <p className={css.text}>
-            Whether you're planning a road trip, a business journey, or just
-            need a reliable set of wheels, we've got you covered.
-          </p>
-          <p className={css.text}>
-            Choose from our wide selection of quality vehicles, book with ease,
-            and hit the road with confidence.
-          </p>
-          <p className={css.text}>
-            Your journey, your way – start your adventure today!
-          </p>
-          <div className={css.buttons}>
-            <button
-              type="button"
-              className={css.rentalCarBtn}
-              onClick={() => window.open(`tel:+${TEL_NUMBER}`)}
-            >
-              Rental car
-            </button>
-            <NavLink to="/catalog" className={css.toCatalogLink}>
-              <button type="button" className={css.toCatalogBtn}>
-                Catalog
-              </button>
-            </NavLink>
+    <>
+      <section className={css.section}>
+        <div className={css.hero}>
+          <div className={css.imagesDiv}>
+            <img
+              src={hero1}
+              alt="hero"
+              className={css.heroImage}
+              style={{ opacity: opacity1 }}
+              id="image1"
+            />
+            <img
+              src={hero2}
+              alt="hero"
+              className={css.heroImage}
+              style={{ opacity: opacity2 }}
+              id="image2"
+            />
+            <img
+              src={hero3}
+              alt="hero"
+              className={css.heroImage}
+              style={{ opacity: opacity3 }}
+              id="image3"
+            />
           </div>
         </div>
+      </section>
+      <div className={css.info} style={{ marginTop: margin }}>
+        <h1>DriveEasy Rental</h1>
+        <h2>The best car rental service in the world </h2>
+        <p className={css.text}>
+          Discover freedom on four wheels with DriveEasy Rental
+        </p>
+        <p className={css.text}>
+          Whether you're planning a road trip, a business journey, or just need
+          a reliable set of wheels, we've got you covered.
+        </p>
+        <p className={css.text}>
+          Choose from our wide selection of quality vehicles, book with ease,
+          and hit the road with confidence.
+        </p>
+        <p className={css.text}>
+          Your journey, your way – start your adventure today!
+        </p>
+        <div className={css.buttons}>
+          <button
+            type="button"
+            className={css.rentalCarBtn}
+            onClick={() => window.open(`tel:+${TEL_NUMBER}`)}
+          >
+            Rental car
+          </button>
+          <NavLink to="/catalog" className={css.toCatalogLink}>
+            <button type="button" className={css.toCatalogBtn}>
+              Catalog
+            </button>
+          </NavLink>
+        </div>
       </div>
-    </section>
+    </>
   );
 };
 
